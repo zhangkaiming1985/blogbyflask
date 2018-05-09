d56e9011231c781c7843c40fd7a6103fe570e5ee
zhangkaiming1985 <34681444+zhangkaiming1985@users.noreply.github.com>
2018-05-09 18:07:51 +0800

    pc

--- hello.py
+++ hello.py
@@ -1,18 +1,65 @@
-from flask import Flask, render_template
-from flask import request
+import os
+
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = 'hard to guess'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+	"""定义Role模型"""
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
 
 
-@app.route('/')
+class User(db.Model):
+	"""定义User模型"""
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+class NameForm(FlaskForm):
+	"""表单类"""
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+
+
+@app.route('/', methods=['get', 'post'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session['name']
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session['name'])
 
 
 @app.route('/user/<name>')
